0a0a7600077ee90fecf1ea76bdfc9564f16e1970
tmelko-vidyo <tmelko@vidyo.com>
2018-12-03 16:59:59 +0200

    - Add permissions checkout before start video page; - Remove strings merge and manual dependency manage; - Add camera privacy while in background and resources release on destroy; - Removed unused menu item.

--- src/android/VidyoIOPlugin.java
+++ src/android/VidyoIOPlugin.java
@@ -1,7 +1,8 @@
 package com.vidyo.plugin;
- 
+
 import android.content.Context;
 import android.content.Intent;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaWebView;
@@ -9,27 +10,47 @@ import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import com.vidyo.vidyoconnector.VidyoIOActivity;
- 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class VidyoIOPlugin extends CordovaPlugin {
- 
+
+    private static final int PERMISSION_REQ_CODE = 0x7b;
+
+    private static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
     }
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         Context context = cordova.getActivity().getApplicationContext();
-        if(action.equals("launchVidyoIO")) {
-            this.openNewActivity(context,args);
+        if (action.equals("launchVidyoIO")) {
+            this.openNewActivity(context, args);
             return true;
         }
         return false;
     }
- 
-    private void openNewActivity(Context context,JSONArray args)  throws JSONException {
+
+    private void openNewActivity(Context context, JSONArray args) throws JSONException {
+        /* Check for required permissions */
+        if (!hasAllPermissions()) {
+            this.cordova.requestPermissions(this, PERMISSION_REQ_CODE, PERMISSIONS);
+            return;
+        }
+
         Intent intent = new Intent(context, VidyoIOActivity.class);
         intent.putExtra("token", args.getString(0));
         intent.putExtra("host", args.getString(1));
@@ -37,7 +58,30 @@ public class VidyoIOPlugin extends CordovaPlugin {
         intent.putExtra("resourceId", args.getString(3));
         intent.putExtra("hideConfig", true);
         intent.putExtra("autoJoin", true);
- 
+
         this.cordova.getActivity().startActivity(intent);
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults) throws JSONException {
+        if (requestCode == PERMISSION_REQ_CODE) {
+            for (int result : grantResults) {
+                if (result == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(cordova.getActivity(), "Permissions are not granted!", Toast.LENGTH_SHORT).show();
+                    return; /* quit */
+                }
+            }
+
+            /* Success */
+            Toast.makeText(cordova.getActivity(), "Permissions granted! Please proceed...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean hasAllPermissions() {
+        for (String permission : PERMISSIONS) {
+            if (!this.cordova.hasPermission(permission)) return false;
+        }
+
+        return true;
     }
 }
