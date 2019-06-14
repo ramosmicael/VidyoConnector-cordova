ef7ae53d37eb9bdc48caa7f6c417a973d2a90c15
ramosmicael <ramosmicael@users.noreply.github.com>
2019-06-14 09:48:42 +0100

    Update VidyoIOPlugin.java

--- src/android/VidyoIOPlugin.java
+++ src/android/VidyoIOPlugin.java
@@ -7,9 +7,12 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.PluginResult;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 import android.view.WindowManager;
 
